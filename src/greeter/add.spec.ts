960a04d8602bfe22f8422b1d3b62a086b2cea648
Mensel123 <mendelengelaer@hotmail.com>
2022-07-14 13:00:21 +0200

    Implement add function taking single parameter

--- src/greeter/add.spec.ts
+++ src/greeter/add.spec.ts
@@ -1,9 +1,8 @@
 import { Add } from "./add";
 
 describe('add', () => {
-  it('should return 0 when input is empty', () => {
-    const result = Add();
-
-    expect(result).toBe(0);
-  })
+  it("should return 0 when input is an empty string", function() {
+    const result = Add('');
+    expect(result).toBe(0)
+  });
 })
--- src/greeter/add.ts
+++ src/greeter/add.ts
@@ -1,3 +1,3 @@
-export function Add() {
+export function Add(numbers: string) {
   return 0;
 }
